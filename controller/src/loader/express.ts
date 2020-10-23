44b0356df2098b32e1bc5b63bfe0759509700291
Justin Bedrich <justin.bedrich@gmail.com>
2020-10-23 19:00:46 +0200

    add express logic

--- controller/src/loader/express.ts
+++ controller/src/loader/express.ts
@@ -2,7 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.settings.env !== 'development'
+	? app.use(express.static('../client/dist'))
+	: app.get('/', (_, res) => res.redirect('http://localhost:8080/'))
+
 app.listen(3000)
 
 export default app
-//TODO: Serve Dist folder from Client.
+//TODO: Serve dist folder from Client.
